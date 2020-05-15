5f92fb35b2fd9660ad1fe24cebbe4ae60c29f235
Nobert-Ok <o.chukwuebu@alustudent.com>
2020-05-15 11:48:51 +0200

    make it better

--- conf.php
+++ conf.php
@@ -4,20 +4,20 @@ if (!isset($_SESSION)) {
 	session_start();
 }
 //connect to db
-$dbhost = "localhost";// this will ususally be 'localhost', but can sometimes differ
-$dbname = "alusm";// the name of the database that you are going to use for this project
-$dbuser = "root";// the username that you created, or were given, to access your database
-$dbpass = "";// the password that you created, or were given, to access your database
+// $dbhost = "localhost";// this will ususally be 'localhost', but can sometimes differ
+// $dbname = "alusm";// the name of the database that you are going to use for this project
+// $dbuser = "root";// the username that you created, or were given, to access your database
+// $dbpass = "";// the password that you created, or were given, to access your database
 
 // $GLOBALS["mysql_hostname"] = "sql12.freemysqlhosting.net";
 // $GLOBALS["mysql_username"] = "sql12340558";
 // $GLOBALS["mysql_password"] = "9ETjVU3WS3";
 // $GLOBALS["mysql_database"] = "sql12340558";
 
-// $dbhost = "sql12.freemysqlhosting.net";// this will ususally be 'localhost', but can sometimes differ
-// $dbname = "sql12340558";// the name of the database that you are going to use for this project
-// $dbuser = "sql12340558";// the username that you created, or were given, to access your database
-// $dbpass = "9ETjVU3WS3";// the password that you created, or were given, to access your database
+$dbhost = "sql12.freemysqlhosting.net";// this will ususally be 'localhost', but can sometimes differ
+$dbname = "sql12340558";// the name of the database that you are going to use for this project
+$dbuser = "sql12340558";// the username that you created, or were given, to access your database
+$dbpass = "9ETjVU3WS3";// the password that you created, or were given, to access your database
 
 $conn      = mysqli_connect($dbhost, $dbuser, $dbpass) or die("MySQL Error with $conn on base.php: " .mysql_error());
 $db_select = mysqli_select_db($conn, $dbname) or die("MySQL Error with $db_select on base.php: " .mysql_error());
--- user/simpleusers/config.inc.php
+++ user/simpleusers/config.inc.php
@@ -7,16 +7,16 @@
 	* Please provide your MySQL login information below.
 	*/
 
-	$GLOBALS["mysql_hostname"] = "localhost";
-	$GLOBALS["mysql_username"] = "root";
-	$GLOBALS["mysql_password"] = "";
-	$GLOBALS["mysql_database"] = "ALUSM";
+	// $GLOBALS["mysql_hostname"] = "localhost";
+	// $GLOBALS["mysql_username"] = "root";
+	// $GLOBALS["mysql_password"] = "";
+	// $GLOBALS["mysql_database"] = "ALUSM";
 
 	// remote db conn
-	// $GLOBALS["mysql_hostname"] = "sql12.freemysqlhosting.net";
-	// $GLOBALS["mysql_username"] = "sql12340558";
-	// $GLOBALS["mysql_password"] = "9ETjVU3WS3";
-	// $GLOBALS["mysql_database"] = "sql12340558";
+	$GLOBALS["mysql_hostname"] = "sql12.freemysqlhosting.net";
+	$GLOBALS["mysql_username"] = "sql12340558";
+	$GLOBALS["mysql_password"] = "9ETjVU3WS3";
+	$GLOBALS["mysql_database"] = "sql12340558";
 
 	// $GLOBALS["mysql_hostname"] = "localhost";
 	// $GLOBALS["mysql_username"] = "nsint_ssplanner";
